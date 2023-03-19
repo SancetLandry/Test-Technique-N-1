6ba50f8021d1f4c5a1c0ceaf3bb814771fed6625
Etudiant O'clock <ToonFAQ@gmail.com>
2023-03-19 14:27:40 +0100

    Reprise du code à 0 + 2 fonctionnalitées OK

--- index.js
+++ index.js
@@ -1,100 +1,57 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+const container = document.getElementById("container");
+const MIN_RECTANGLE_SIZE = 10;
+const rectangles = [];
 
-let isDrawing = false;
-let startX;
-let startY;
-let rectangles = []; // tableau pour stocker les rectangles
+container.addEventListener("mousedown", function(event) {
+    const color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    const startX = event.clientX;
+    const startY = event.clientY;
+    let rectangle = null;
 
-canvas.addEventListener("mousedown", (e) => {
-  startX = e.offsetX;
-  startY = e.offsetY;
-  isDrawing = true;
-});
+    container.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseup", onMouseUp);
 
-canvas.addEventListener("mousemove", (e) => {
-  if (!isDrawing) return;
-  const currentX = e.offsetX;
-  const currentY = e.offsetY;
-  const width = currentX - startX;
-  const height = currentY - startY;
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-  const color = `rgb(${red},${green},${blue})`;
-  ctx.fillStyle = color;
-  ctx.fillRect(startX, startY, width, height);
+    function onMouseMove(event) {
+        const width = Math.abs(event.clientX - startX);
+        const height = Math.abs(event.clientY - startY);
 
-  // ajouter le rectangle au tableau des rectangles
-  rectangles.push({x: startX, y: startY, width: width, height: height, color: color});
-});
+        if (width < MIN_RECTANGLE_SIZE || height < MIN_RECTANGLE_SIZE) {
+            event.preventDefault();
+            return;
+        }
 
-canvas.addEventListener("mouseup", () => {
-  isDrawing = false;
-});
+        if (rectangle) {
+            container.removeChild(rectangle); // Si un rectangle est déjà dessiné, on le supprime
+        }
 
-canvas.addEventListener("dblclick", (e) => {
-  const x = e.offsetX;
-  const y = e.offsetY;
+        rectangle = document.createElement("div");
+        rectangle.classList.add("rectangle");
+        rectangle.style.width = width + "px";
+        rectangle.style.height = height + "px";
+        rectangle.style.backgroundColor = color;
+        rectangle.style.top = Math.min(event.clientY, startY) + "px";
+        rectangle.style.left = Math.min(event.clientX, startX) + "px";
 
-  rotateRectangle(x, y);
-
-  // boucle à travers tous les rectangles pour vérifier si le clic est à l'intérieur d'un rectangle
-  for (let i = 0; i < rectangles.length; i++) {
-    const rect = rectangles[i];
-    if (x > rect.x && x < rect.x + rect.width && y > rect.y && y < rect.y + rect.height) {
-      // si le clic est à l'intérieur d'un rectangle, rotate de 360°
-      ctx.translate(rect.x + rect.width/2, rect.y + rect.height/2);
-      ctx.rotate(2*Math.PI);
-      ctx.translate(-(rect.x + rect.width/2), -(rect.y + rect.height/2));
-      rectangles.splice(i, 1); // supprimer le rectangle tourné du tableau des rectangles
+        container.appendChild(rectangle);
     }
-  }
+
+    function onMouseUp(event) {
+        container.removeEventListener("mousemove", onMouseMove);
+        document.removeEventListener("mouseup", onMouseUp);
+        rectangles.push(rectangle);
+    }
 });
 
-function rotateRectangle(x, y) {
-    // boucle à travers tous les rectangles pour vérifier si le clic est à l'intérieur d'un rectangle
-    for (let i = 0; i < rectangles.length; i++) {
-      const rect = rectangles[i];
-      if (x > rect.x && x < rect.x + rect.width && y > rect.y && y < rect.y + rect.height) {
-        // si le clic est à l'intérieur d'un rectangle, rotate de 360°
-        const centerX = rect.x + rect.width/2;
-        const centerY = rect.y + rect.height/2;
-        const startAngle = 0;
-        const endAngle = Math.PI * 2;
-        const rotationSpeed = Math.PI / 120; // 1 degré par frame
-        let currentAngle = startAngle;
-  
-        ctx.fillStyle = rect.color; // définir la couleur de remplissage avant d'entrer dans la boucle animate()
-  
-        function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height); // effacer tout le canvas
-  ctx.save(); // sauvegarder l'état du contexte
-  ctx.translate(centerX, centerY);
-  ctx.rotate(currentAngle);
-  ctx.translate(-centerX, -centerY);
-  ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
-  ctx.restore(); // restaurer l'état du contexte
-  currentAngle += rotationSpeed;
-
-  if (currentAngle < endAngle) {
-    requestAnimationFrame(animate);
-  } else {
-    // Supprimer le rectangle tourné du tableau des rectangles
-    const index = rectangles.findIndex(r => r === rect);
-    if (index !== -1) rectangles.splice(index, 1);
-
-    // réinitialiser le canvas avec tous les rectangles restants
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let j = 0; j < rectangles.length; j++) {
-      const rect = rectangles[j];
-      ctx.fillStyle = rect.color;
-      ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+container.addEventListener("dblclick", function(event) {
+    const index = rectangles.indexOf(event.target);
+    if (index !== -1 && event.target === rectangles[index]) {
+        const rectangle = rectangles[index];
+        rectangle.style.transform = "rotate(360deg)";
+        setTimeout(function() {
+            if (rectangle.offsetWidth > 10 && rectangle.offsetHeight > 10) {
+                rectangle.remove();
+                rectangles.splice(index, 1);
+            }
+        }, 1000);
     }
-  }
-}
-  
-        animate(); // lancer l'animation
-      }
-    }
-  };
+});
